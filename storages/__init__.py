6fea6074caff9d7957cc4d205b19ea0078e62497
shan <3180458409@qq.com>
2025-05-08 09:39:15 +0800

    初始化仓库20250508...

---
+++ storages/__init__.py
@@ -0,0 +1,1 @@
+from exceptions.empty import PoolEmptyException
---
+++ storages/redis.py
@@ -0,0 +1,13 @@
+import redis
+from exceptions.empty import PoolEmptyException
+import sys
+
+sys.path.append(".")
+REDIS_CLIENT_VERSION = redis.__version__
+IS_REDIS_VERSION_2 = REDIS_CLIENT_VERSION.startswith('2.')
+
+
+# class RedisClient(object):
+#     def __init__(self, host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, **kwargs):
+#         self.db = redis.StrictRedis(host=host, port=port, password=password, decode_responses=True, **kwargs)
+print(IS_REDIS_VERSION_2)
